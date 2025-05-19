38869201495dda9305be07633a4e759103d77ff7
anssikaki <anssi.kaki@upm.com>
2025-05-19 11:34:48 +0300

    commented surprise_app.py

--- surprise_app.py
+++ surprise_app.py
@@ -4,10 +4,10 @@ import openai
 openai.api_key = st.secrets["openai"]["api_key"]
 
 # response = openai.ChatCompletion.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "user", "content": "Summarize this text: " + input_text}
-    ]
-)
+#    model="gpt-4o",
+#    messages=[
+#        {"role": "user", "content": "Summarize this text: " + input_text}
+#    ]
+#)
 
 # st.write(response.choices[0].message.content)
