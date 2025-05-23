194c17aaefbbeddbb3a2f4dee8b31a0605f81dda
anssikaki <anssi.kaki@upm.com>
2025-05-23 10:47:46 +0300

    Simple surprise_app.py

--- surprise_app.py
+++ surprise_app.py
@@ -3,6 +3,7 @@ import logging
 import streamlit as st
 from openai import OpenAI
 
+st.write("test app")
 # Example syntax for using OpenAI API
 # api_key = st.secrets.get("openai", {}).get("api_key") or os.getenv("OPENAI_API_KEY")
 # client = OpenAI(api_key=api_key)
